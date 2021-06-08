b5ec2d6d2b27f669ba8d40cb4ab6628fac20579f
ktapia576 <ktapia576@hotmail.com>
2021-06-08 17:31:06 -0400

    Add Link Components

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,12 @@
 import * as React from 'react';
+import { Link } from 'gatsby';
 
 function AboutPage(){
     return (
         <main>
             <title>About</title>
             <h1>About Us</h1>
+            <Link to="/">Home Page</Link>
             <p>This is to test the about page for the website.</p>
         </main>
     );
