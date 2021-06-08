97ef208b61d746f586d6d9211af2ab2187af6e67
ktapia576 <ktapia576@hotmail.com>
2021-06-08 05:27:06 -0400

    Add about page

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+function AboutPage(){
+    return (
+        <main>
+            <title>About</title>
+            <h1>About Us</h1>
+            <p>This is to test the about page for the website.</p>
+        </main>
+    );
+}
+
+export default AboutPage; 
