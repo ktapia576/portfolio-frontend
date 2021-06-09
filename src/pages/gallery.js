bee197c3e50ec1b5c49dbc28a44e009e8f0bdc09
ktapia576 <ktapia576@hotmail.com>
2021-06-08 23:37:34 -0400

    Add gallery page and frame component

---
+++ src/pages/gallery.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+import Frame from '../components/frame'
+
+const GalleryPage = () => {
+    return (
+        <Frame>
+          <p>This will be passed in as children</p>
+        </Frame>
+    );
+}
+
+
+export default GalleryPage;
