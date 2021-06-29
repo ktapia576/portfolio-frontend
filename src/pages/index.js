f5e80a1b27c0afbcf523c31eea316c0ebb2d8dd3
ktapia576 <ktapia576@hotmail.com>
2021-06-29 17:06:47 -0400

    Add bulma styling to index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,17 @@
 import * as React from 'react';
+import '../styles/styles.scss';
 
 const IndexPage = () => {
   return (
-    <>
-      <h1> Hey I'm Kevin Tapia </h1>
-      <h4>Innovative and adaptable Software Developer currently based in Union, New Jersey <br/>
-          Check out my projects and contact me to make something great!
-      </h4>
-      <button>View Projects</button>
-    </>
+    <div className="hero is-fullheight">
+      <div className="hero-body is-flex-direction-column is-justify-content-center">
+        <h1 className="title"> Hey I'm Kevin Tapia </h1>
+        <h4 className="subtitle">Innovative and adaptable Software Developer currently based in Union, New Jersey <br/>
+            Check out my projects and contact me to make something great!
+        </h4>
+        <button className="button">View Projects</button>
+      </div>
+    </div>
   );
 }
 
