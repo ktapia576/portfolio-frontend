249a4e295611b8d2bc20838e61dbcc5cc619f6d8
ktapia576 <ktapia576@hotmail.com>
2021-06-29 15:40:00 -0400

    Add content for index page

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Portfolio - Kevin Tapia`,
+    description: `Kevin's Portfolio to check out his work and experience!`,
+    author: `@ktapia576`,
   },
   plugins: [
     `gatsby-plugin-image`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,13 @@ import * as React from 'react';
 
 const IndexPage = () => {
   return (
-    <h1> Hey I'm Kevin Tapia </h1>
+    <>
+      <h1> Hey I'm Kevin Tapia </h1>
+      <h4>Innovative and adaptable Software Developer currently based in Union, New Jersey <br/>
+          Check out my projects and contact me to make something great!
+      </h4>
+      <button>View Projects</button>
+    </>
   );
 }
 
