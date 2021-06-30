66301de479f92d9e4b3e2cdc1ce365a1ef060ac3
ktapia576 <ktapia576@hotmail.com>
2021-06-30 16:46:10 -0400

    Add test projects page

---
+++ src/pages/projects.js
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+function ProjectsPage(){
+    return (
+        <p>This is to test the projects page for the website.</p>
+    );
+}
+
+export default ProjectsPage; 
