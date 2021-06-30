0f2c1933c04b38fbc18c44eb4bdbf15bc63318cf
ktapia576 <ktapia576@hotmail.com>
2021-06-30 16:46:25 -0400

    Add styling and routing for projects page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,17 @@
 import * as React from 'react';
+import { Link } from 'gatsby';
 import '../styles/styles.scss';
 
 const IndexPage = () => {
   return (
-    <div className="hero is-fullheight">
-      <div className="hero-body is-flex-direction-column is-justify-content-center">
-        <h1 className="title"> Hey I'm Kevin Tapia </h1>
-        <h4 className="subtitle">Innovative and adaptable Software Developer currently based in Union, New Jersey <br/>
-            Check out my projects and contact me to make something great!
+    <div className="hero has-background-black-ter is-fullheight">
+      <div className="hero-body has-text-centered is-flex-direction-column is-justify-content-center">
+        <h1 className="title has-text-grey-lighter"> Hello, my name is Kevin Tapia. </h1>
+        <h4 className=" has-text-grey mb-5"> 
+          Innovative and adaptable Software Developer currently based in Union, New Jersey. <br/>
+          Check out my projects and contact me to make something great!
         </h4>
-        <button className="button">View Projects</button>
+        <Link to="/projects" className="button is-light is-outlined">View Projects</Link>
       </div>
     </div>
   );
