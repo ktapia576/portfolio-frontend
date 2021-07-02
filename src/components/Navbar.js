f364b8218b5f16e2729f67122fe9f6d6805b65fb
ktapia576 <ktapia576@hotmail.com>
2021-07-02 03:42:21 -0400

    Update navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
 const Navbar = () => {
   return (
-    <div className="navbar" role="navigation" aria-label="main navigation">
-        
-    </div>
+    <nav className="navbar has-background-black-ter is-dark" role="navigation" aria-label="main navigation">
+        <div className="navbar-brand">
+            <Link to="/" className="navbar-item">Home</Link>
+
+            <a href="/#" role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+            </a>
+        </div>
+    </nav>
   )
 }
 
