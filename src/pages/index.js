6ff7b379ad1a084eca5ac034fb7ea88ddb2597b9
ktapia576 <ktapia576@hotmail.com>
2021-07-07 23:31:05 -0400

    Update small bug for hot relaoding by capitalizing Navbar in import

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Link } from 'gatsby';
-import Navbar from '../components/navbar';
+import Navbar from '../components/Navbar';
 import '../styles/styles.scss';
 
 const IndexPage = () => {
