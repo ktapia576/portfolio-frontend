23262afdeb993d66afa740f2a8964ad839aeaaed
ktapia576 <ktapia576@hotmail.com>
2021-07-15 23:41:38 -0400

    Update class to className

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer class="footer has-text-grey-lighter">
-        <div class="content has-text-centered">
+    <footer className="footer has-text-grey-lighter">
+        <div className="content has-text-centered">
             <p>
                 Portfolio created by Kevin Tapia (me). Source <a href="https://github.com/ktapia576/portfolio-frontend">Code</a>.
                 This website is built from the ground up using React,js, Gatsby, Node.js, hosted on Digital Ocean Droplet.
